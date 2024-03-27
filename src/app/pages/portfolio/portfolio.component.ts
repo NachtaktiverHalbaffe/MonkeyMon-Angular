3d28d1264c7cd8cfb725c5f844c9866795d05e22
Felix Brugger <LeMonkay.VTIT@tutanota.com>
2024-03-27 13:32:05 +0000

    added portoflio iframes

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { TextComponent } from '../../components/text/text.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, TextComponent],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.css'
+  styleUrl: './portfolio.component.css',
 })
-export class PortfolioComponent {
-
-}
+export class PortfolioComponent {}
