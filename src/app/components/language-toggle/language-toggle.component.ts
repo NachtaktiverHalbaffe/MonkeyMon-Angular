ac82b19a4d2d73d18ad5f2a81b2fac1cde7065ea
Felix Brugger <LeMonkay.VTIT@tutanota.com>
2024-04-02 12:59:29 +0000

    Split language toggle out

---
+++ src/app/components/language-toggle/language-toggle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-toggle',
+  standalone: true,
+  imports: [],
+  templateUrl: './language-toggle.component.html'
+})
+export class LanguageToggleComponent {
+
+}
--- src/app/views/navbar/navbar.component.ts
+++ src/app/views/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ThemeToggleComponent } from '../../components/theme-toggle/theme-toggle.component';
+import { LanguageToggleComponent } from '../../components/language-toggle/language-toggle.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, ThemeToggleComponent],
+  imports: [RouterModule, ThemeToggleComponent, LanguageToggleComponent],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {}
