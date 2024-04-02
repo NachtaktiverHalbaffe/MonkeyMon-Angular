a498793944a4e2dffcd62d071875974c169803ad
Felix Brugger <LeMonkay.VTIT@tutanota.com>
2024-04-02 12:54:29 +0000

    Only render dev tools if dev mode is on

--- src/app/components/angular-query-devtools/angular-query-devtools.component.ts
+++ src/app/components/angular-query-devtools/angular-query-devtools.component.ts
@@ -1,16 +1,20 @@
 import { AngularQueryDevtools } from '@tanstack/angular-query-devtools-experimental';
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 
 @Component({
   selector: 'app-angular-query-devtools',
   standalone: true,
   imports: [AngularQueryDevtools],
   template: `
+    @if(isDevMode){
     <angular-query-devtools
       [initialIsOpen]="false"
       buttonPosition="bottom-right"
       position="bottom"
     />
+    }
   `,
 })
-export class AngularQueryDevtoolsComponent {}
+export class AngularQueryDevtoolsComponent {
+  isDevMode: boolean = isDevMode();
+}
